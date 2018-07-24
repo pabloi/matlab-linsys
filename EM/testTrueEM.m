53a34f114c7b18b6af648e10e3eaba8a7cc90b22
Unknown <itu.pablo@gmail.com>
2018-07-23 21:47:20 -0400

    Changed testScript

--- EM/testTrueEM.m
+++ EM/testTrueEM.m
@@ -2,11 +2,11 @@
 addpath(genpath('../')) %Adding the matlab-sysID toolbox to path, just in case
 %% Create model:
 D1=2;
-D2=100;
+D2=180;
 N=1000;
 A=randn(D1);
 A=.98*A./max(abs(eig(A))); %Setting the max eigenvalue to .98
-A=[.95,0;0,.99];
+A=[.97,0;0,.995];
 A=jordan(A); %Using A in its jordan canonical form so we can compare identified systems, WLOG
 %B=3*randn(D1,1);
 %B=B./sign(B); %Forcing all elements of B to be >0, WLOG
@@ -14,7 +14,7 @@ B=(eye(size(A))-A)*ones(size(A,1),1); %WLOG, arbitrary scaling
 U=[zeros(300,1);ones(N,1);zeros(N/2,1)]'; %Step input and then removed
 C=randn(D2,D1);
 D=randn(D2,1);
-Q=eye(D1)*.0005;
+Q=eye(D1)*.00005;
 R=eye(D2)*.01;
 
 %% Simulate
