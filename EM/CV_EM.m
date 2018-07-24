c04115abe8245f640312333623549720d0234db2
Unknown <itu.pablo@gmail.com>
2018-07-23 21:31:18 -0400

    Added comments and canonization to CV_EM

--- EM/CV_EM.m
+++ EM/CV_EM.m
@@ -7,23 +7,25 @@ for i=1:Nfolds
     %Transform A, B to describe dynamics for every sample, instead of every Nfolds samples.
     A=A^(1/Nfolds);
     I=eye(size(A));
-    B=(I-A^Nfolds)\((I-A)*B);
+    B=(I-A^Nfolds)\((I-A)*B); %Changing B such that the steady state response to a step input is unchanged.
     %newX0=A^(i-1) * X(:,1) + (I-A)\(I-A^(i-1))*B;
     %[B,D,X]=chngInitState(A,B,C,D,X,newX0);
     
     %Re-compute X to get a state estimate for every sample, instead of 1-every-Nfolds
-    Xn=nan(size(X,1),ceil(size(Y,1)/Nfolds)*Nfolds);
+    [X,P,Xf,Pf,Xp,Pp,rejSamples]=statKalmanSmoother(Y,A,C,Q,R,[],[],B,D,U);
+    Xn=nan(size(X,1),ceil(size(Y,2)/Nfolds)*Nfolds);
     for k=1:Nfolds
        if k==i
           Xn(:,[k:Nfolds:size(X,2)*Nfolds])=X;
        else
-          Xn(:,[k:Nfolds:size(X,2)*Nfolds])=A^(k-i) * X + (I-A)\(I-A^(k-i))*B; 
+          Xn(:,[k:Nfolds:size(Y,2)])=A^(k-i) * X(:,1:floor(size(Y,2)/Nfolds)) + B*U(:,[k:Nfolds:size(Y,2)]); 
        end
     end
-    X=Xn(:,1:size(Y,1));
     
+    %% Because we want to compare across the different cross-fittings, we need to agree on a canonical form:
+    [J,B,C,X,~,Q] = canonizev2(A,B,C,Xn,Q);
     %%
-    model.J=A;
+    model.J=J;
     model.B=B;
     model.C=C;
     model.D=D;
