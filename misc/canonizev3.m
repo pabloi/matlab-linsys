3211164321e223086496f0c2b572a39d4f5d7ec3
Unknown <itu.pablo@gmail.com>
2018-08-04 12:31:53 -0400

    Added alternative canonization

--- misc/canonizev3.m
+++ misc/canonizev3.m
@@ -1,4 +1,4 @@
-function [J,K,C,X,V,Q] = canonizev2(A,B,C,Xa,Q)
+function [J,K,C,X,V,Q] = canonizev3(A,B,C,Xa,Q)
 %Canonize returns the canonical form of the linear system given by
 %A,B,C,D,X; scaling C to have unity norm along each column
 
@@ -23,15 +23,9 @@ function [J,K,C,X,V,Q] = canonizev2(A,B,C,Xa,Q)
 if nargin<5
     Q=zeros(size(J));
 end
-%[J,K,C,X,V,Q] = canonizev3(A,B,C,Xa,Q);
-%return
-
-
 %Lazy way:
 sys=ss(A,B,C,zeros(size(C,1),size(B,2)),1);
 [csys,V]=canon(sys);
-J=csys.A;
-K=csys.B;
 
 %Scale B at will
 %TODO: formally, we can only scale the states whose evolution depends
@@ -41,8 +35,8 @@ K=csys.B;
 %m=max(abs(K),[],2);
 %s=sign(K(abs(K)==m));
 %scale=(eye(size(J))-J)*ones(size(J,1),1).*s./m;
-scale=(eye(size(J))-J)\K(:,1);
-V2=diag(1./scale);
+scale=sum(csys.C.^2,1).*sign(max(csys.B,[],2))';
+V2=diag(scale);
 csys = ss2ss(csys,V2); 
 J=csys.A;
 K=csys.B;
