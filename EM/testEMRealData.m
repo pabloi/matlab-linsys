11240e17aa03fd9449ce3f9cd32e5fb5718ed348
Unknown <itu.pablo@gmail.com>
2018-08-14 16:27:56 -0400

    Improved test on EMG (real) data

--- EM/testEMRealData.m
+++ EM/testEMRealData.m
@@ -29,8 +29,11 @@ U=[zeros(size(dataSym{1},1),1);ones(size(dataSym{2},1),1);zeros(size(dataSym{3},
 %%
 Y=medfilt1([median(dataSym{1},3); median(dataSym{2},3)],3)';
 U=[zeros(size(dataSym{1},1),1);ones(size(dataSym{2},1),1)]';
-%% Identify 0: handcrafted
-D1=2;
+%%
+Y=medfilt1([median(data{1},3); median(data{2},3)],3)';
+U=[zeros(size(data{1},1),1);ones(size(data{2},1),1)]';
+%% Identify 0: handcrafted sPCA
+D1=3;
 [model] = sPCAv8(Y(:,51:950)',D1,[],[],[]);
 A=model.J;
 C=model.C;
@@ -46,17 +49,6 @@ slogLh=dataLogLikelihood(Y,U,A,B,C,D,Q,R,X(:,1),Q);
 norm(Y-C*X-D*U,'fro')
 %% Assuming these are the 'real' params, find the MLE states
 [Xs,Ps,Pt,Xf,Pf,rejSamples]=statKalmanSmoother(Y,A,C,Q,R,[],[],B,D,U);
-%%
-figure; hold on;
-plot(X','k','LineWidth',2)
-set(gca,'ColorOrderIndex',1)
-plot(Xf')
-plot(Xs')
-set(gca,'ColorOrderIndex',1)
-cc=get(gca,'ColorOrder');
-for i=1:size(X,1)
-    patch([1:size(Xf,2),size(Xf,2):-1:1],[Xf(i,:)'+sqrt(squeeze(Pf(i,i,:)));flipud(Xf(i,:)'-sqrt(squeeze(Pf(i,i,:))))]',cc(i,:),'EdgeColor','none','FaceAlpha',.3)
-end
 %% Identify 1: true EM with smooth start
 tic
 [fAh,fBh,fCh,fDh,fQh,fRh,fXh,fPh]=trueEM(Y,U,Xs);
@@ -77,31 +69,69 @@ logLh=dataLogLikelihood(Y,U,Jh,Kh,Ch,Dh,Qh,Rh,Xh(:,1),Ph(:,:,1));
 M=size(fXh,1);
 x0=zeros(M,1);
 figure;
-[pp,cc,aa]=pca(Y,'Centered','off');
 
+% Plot output PCs and residuals
+[cc,pp,aa]=pca(Y','Centered','off');
 for kk=1:M+2
-subplot(M+3,2,(kk-1)*2+1) %Output along first PC of true data
+subplot(M+4,4,(kk-1)*4+1) %Output along first PC of true data
 hold on
-plot(cc(:,kk)'*(Ch*Xh+Dh*U),'LineWidth',1)
-plot(cc(:,kk)'*(fCh*fXh+fDh*U),'LineWidth',1)
-plot(cc(:,kk)'*(C*X+D*U),'LineWidth',1)
+p1=plot(cc(:,kk)'*(Ch*Xh+Dh*U),'LineWidth',1);
+p2=plot(cc(:,kk)'*(fCh*fXh+fDh*U),'LineWidth',1);
+p3=plot(cc(:,kk)'*(C*X+D*U),'LineWidth',1);
 plot(cc(:,kk)'*(Y),'k','LineWidth',1)
 if kk==1
     title('Output projection over main PCs')
 
 end
+ylabel(['PC ' num2str(kk)])
 end
+subplot(M+4,6,(kk+1)*6+1)
+hold on
+title('First PC of residual')
+[pp,cc,aa]=pca((Y-Ch*Xh-Dh*U)','Centered','off');
+plot(cc(:,1),'LineWidth',1)
+[pp,cc,aa]=pca((Y-fCh*fXh-fDh*U)','Centered','off');
+plot(cc(:,1),'LineWidth',1)
+[pp,cc,aa]=pca((Y-C*X-D*U)','Centered','off');
+plot(cc(:,1),'LineWidth',1)
+subplot(M+4,6,(kk+1)*6+2)
+hold on
+title('First PC of residual')
+[pp,cc,aa]=pca((Y-Ch*Xh-Dh*U)','Centered','off');
+%histogram(cc(:,1))
+qq1=qqplot(cc(:,1));
+qq1(1).MarkerEdgeColor=p1.Color;
+[pp,cc,aa]=pca((Y-fCh*fXh-fDh*U)','Centered','off');
+%histogram(cc(:,1))
+qq1=qqplot(cc(:,1));
+qq1(1).MarkerEdgeColor=p2.Color;
+[pp,cc,aa]=pca((Y-C*X-D*U)','Centered','off');
+%histogram(cc(:,1))
+qq1=qqplot(cc(:,1));
+qq1(1).MarkerEdgeColor=p3.Color;
+subplot(M+4,6,(kk+1)*6+3)
+hold on
+title('First PC of residual')
+[pp,cc,aa]=pca((Y-Ch*Xh-Dh*U)','Centered','off');
+plot(fftshift(xcorr(cc(:,1))))
+[pp,cc,aa]=pca((Y-fCh*fXh-fDh*U)','Centered','off');
+plot(fftshift(xcorr(cc(:,1))))
+[pp,cc,aa]=pca((Y-C*X-D*U)','Centered','off');
+plot(fftshift(xcorr(cc(:,1))))
+axis tight
+aa=axis;
+axis([1 10 aa(3:4)])
+
 
 [Y2,X2]=fwdSim(U,Jh,Kh,Ch,Dh,x0,[],[]);
 [Y3,X3]=fwdSim(U,fJh,fKh,fCh,fDh,x0,[],[]);
-
 for i=1:M
-subplot(M+3,2,2*i) %States
+subplot(M+4,4,4*i-2) %States
 hold on
 %Smooth versions
 set(gca,'ColorOrderIndex',1)
-p2=plot(X2(i,:),'LineWidth',2,'DisplayName','trueEM');
-p3=plot(X3(i,:),'LineWidth',2,'DisplayName','fastEM');
+p2=plot(X2(i,:),'LineWidth',2,'DisplayName','trueEM (iter)');
+p3=plot(X3(i,:),'LineWidth',2,'DisplayName','sPCA+trueEM');
 p1=plot(X(i,:),'LineWidth',2,'DisplayName','sPCA');
 axis([0 2000 -.5 1.5])
 %Fitted versions:
@@ -113,9 +143,10 @@ patch([1:size(Xh,2),size(Xh,2):-1:1]',[Xh(i,:)+sqrt(squeeze(Ph(i,i,:)))', fliplr
 %patch([1:size(fXh,2),size(fXh,2):-1:1]',[fXh(i,:)+squeeze(fPh(i,i,:))', fliplr(fXh(i,:)-squeeze(fPh(i,i,:))')]',p3.Color,'EdgeColor','none','FaceAlpha',.3)
 title('States')
 legend([p2 p3 p1])
+ylabel(['State ' num2str(i)])
 end
 
-subplot(M+3,2,2*M+4) %Smooth output RMSE
+subplot(M+4,4,4*(M+1)-2) %Smooth output RMSE
 hold on
 %aux=sqrt(sum((Y-Y1).^2));
 %plot(aux)
@@ -125,7 +156,7 @@ aux2=sqrt(sum((Y-Y3).^2));
 plot(aux2,'LineWidth',1)
 aux0=sqrt(sum((Y-C*X-D*U).^2));
 plot(aux0,'LineWidth',1)
-title('Smooth output error (RMSE)')
+title('Smooth output error (Y-CX_s-DU,RMSE)')
 set(gca,'ColorOrderIndex',1)
 %bar1=bar([1900],mean([aux]),'EdgeColor','none','BarWidth',100);
 %text(1600,4,['LogL=' num2str(logL)],'Color',bar1.FaceColor)
@@ -138,7 +169,7 @@ text(1900,.8,['LogL=' num2str(slogLh)],'Color',bar0.FaceColor)
 axis([0 2200 .0 1.5])
 grid on
 
-subplot(M+3,2,2*M+5) %MLE state output error
+subplot(M+4,4,4*(M+2)-2) %MLE state output error
 hold on
 %aux=sqrt(sum((Y-Y1).^2));
 %plot(aux)
@@ -148,7 +179,7 @@ aux2=sqrt(sum((Y-fCh*fXh-fDh*U).^2));
 plot(aux2,'LineWidth',1)
 aux0=sqrt(sum((Y-C*X-D*U).^2));
 plot(aux0,'LineWidth',1)
-title('MLE state-based output error (RMSE)')
+title('MLE state-based output error (Y-CX-DU,RMSE)')
 set(gca,'ColorOrderIndex',1)
 %bar1=bar([1900],mean([aux]),'EdgeColor','none','BarWidth',100);
 %text(1600,4,['LogL=' num2str(logL)],'Color',bar1.FaceColor)
@@ -161,7 +192,7 @@ text(1900,.8,['LogL=' num2str(slogLh)],'Color',bar0.FaceColor)
 axis([0 2200 .0 1.5])
 grid on
 
-subplot(M+3,2,2*M+6) %MLE state innovation
+subplot(M+4,4,4*(M+3)-2) %MLE state innovation
 hold on
 %aux=sqrt(sum((Y-Y1).^2));
 %plot(aux)
@@ -171,7 +202,7 @@ aux2=sqrt(sum((fXh(:,2:end)-fJh*fXh(:,1:end-1)-fKh*U(:,1:end-1)).^2));
 plot(aux2,'LineWidth',1)
 aux0=sqrt(sum((X(:,2:end)-A*X(:,1:end-1)-B*U(:,1:end-1)).^2));
 plot(aux0,'LineWidth',1)
-title('MLE state-based innovation (RMSE)')
+title('MLE state-based innovation (X_+-AX-BU,RMSE)')
 set(gca,'ColorOrderIndex',1)
 %bar1=bar([1900],mean([aux]),'EdgeColor','none','BarWidth',100);
 %text(1600,4,['LogL=' num2str(logL)],'Color',bar1.FaceColor)
