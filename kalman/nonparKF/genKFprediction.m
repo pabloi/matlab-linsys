154e0985b884561cba646e57d5b9e37f22462dac
Pablo Iturralde <itu.pablo@gmail.com>
2018-09-08 21:06:00 -0400

    Added comments to non-parametric Kalman engine

--- kalman/nonparKF/genKFprediction.m
+++ kalman/nonparKF/genKFprediction.m
@@ -1,5 +1,11 @@
 function predictedStateDistr = genKFprediction(priorStateDistr,nextStateGivenCurrDistr)
-%Prediction step update of numerical kalman filter
+%Prediction step update of numerical kalman filter.
+%Implements: p(x_{k+1}|)=\int p(x_{k+1}|x_k)p(x_k) dx_k
+%INPUT
+%nextStateGivenCurrDistr: p(x_{k+1}|x_k) [TRANSITION MATRIX]
+%priorStateDistr: p(x_k) [PRIOR STATE ESTIMATE]
+%OUTPUT:
+%predictedStateDistr: p(x_{k+1}) [PREDICTED STATE]
+
 predictedStateDistr=sum(nextStateGivenCurrDistr .* priorStateDistr,2)';
 end
-
--- kalman/nonparKF/genKFupdate.m
+++ kalman/nonparKF/genKFupdate.m
@@ -1,10 +1,15 @@
 function [updatedStateDistr] = genKFupdate(priorStateDistr,obsGivenStateDistr)
 %Update step of kalman filter, accepting a generic discrete observation
-%probability matrix
+%probability matrix. Implements: p(x_k|y_k) = p(y_k|x_k)p(x_k)/p(y_k)
+%INPUT:
+%priorStateDistr: p(x_k|{y_{k-1}}) [PRIOR STATE ESTIMATE]
+%obsGivenStateDistr: p(y_k|x_k) [OBSERVATION MATRIX]
+%OUTPUT:
+%updatedStateDistr: p(x_k|y_k) [UPDATED STATE]
+
 updatedStateDistr=normalize(obsGivenStateDistr.*priorStateDistr);
 end
 
 function p=normalize(p)
     p=p/sum(p(:));
 end
-
