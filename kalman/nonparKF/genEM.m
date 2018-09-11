08b41a133b07f644d56af52fc4c7a1198a234ef9
Unknown <itu.pablo@gmail.com>
2018-09-11 19:24:02 -0400

    Improved genEM

--- kalman/nonparKF/genEM.m
+++ kalman/nonparKF/genEM.m
@@ -1,18 +1,28 @@
-function genEM(observationHistory,p0)
+function [observationMatrix,transitionMatrix]=genEM(observationHistory,p0,O,T)
 
 D=numel(p0);
 [N,D1]=size(observationHistory);
 %Init:
+if nargin<3
 observationMatrix=randn(D1,D);
-transitionMatrix=randn(D);
-
+else
+  observationMatrix=O;
+end
+if nargin<4
+  transitionMatrix=randn(D);
+else
+  transitionMatrix=T;
+end
+%Check: if observatinHistory is not an integer, discretize
 %iterate
-while
+i=0;
+while i<1%100
   %Estep:
-  [~,~, stateDistrHistory] = genKFstationaryInference(observation,observationMatrix,transitionMatrix,p0);
+  [~,~, stateDistrHistory] = genKFstationaryInference(observationHistory,observationMatrix,transitionMatrix,p0);
   %Mstep:
   [transitionMatrix,observationMatrix] = genParamEstim(stateDistrHistory,observationHistory);
   %Test for improvement:
-  logl=genLogL(observationHistory,observationMatrix,transitionMatrix,stateDistrHistory(1,:));
+  %logl=genLogL(observationHistory,observationMatrix,transitionMatrix,stateDistrHistory(1,:));
+  i=i+1
 end
 end
