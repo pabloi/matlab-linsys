cea5f83678ec994b4c8c563255546dda994d4928
Unknown <itu.pablo@gmail.com>
2018-10-17 17:52:53 -0400

    Added catch statements to randomStartEM for numerical issues

--- EM/randomStartEM.m
+++ EM/randomStartEM.m
@@ -4,8 +4,11 @@ function [A,B,C,D,Q,R,X,P,bestLL]=randomStartEM(Y,U,nd,Nreps,opts)
 fprintf(['\n Starting rep 0... \n']);
 opt1=opts;
 opt1.fastFlag=0; %Enforcing fast filtering
-[A,B,C,D,Q,R,X,P,bestLL]=EM(Y,U,nd,opt1);
-nd=size(X,1);
+try
+    [A,B,C,D,Q,R,X,P,bestLL]=EM(Y,U,nd,opt1);
+catch
+    bestLL=-Inf;
+end
 N=size(Y,2);
 opts.targetLogL=bestLL;
 for i=1:Nreps
@@ -36,15 +39,17 @@ for i=1:Nreps
     %Xguess=medfilt1(Xguess,9,[],2);
 
     %Optimize:
-    [Ai,Bi,Ci,Di,Qi,Ri,Xi,Pi,logl]=EM(Y,U,Xguess,opts);
-    %logl=dataLogLikelihood(Y,U,Ai,Bi,Ci,Di,Qi,Ri,Xi(:,1),Pi(:,:,1));
-
-    %If solution improved, save and display:
-    if logl>bestLL
-        A=Ai; B=Bi; C=Ci; D=Di; Q=Qi; R=Ri; X=Xi; P=Pi;
-        bestLL=logl;
-        opts.targetLogL=bestLL;
-        disp(['Success, best logL=' num2str(bestLL,8)])
+    try %Sometimes ill conditioned problems fail
+      [Ai,Bi,Ci,Di,Qi,Ri,Xi,Pi,logl]=EM(Y,U,Xguess,opts);
+      %If solution improved, save and display:
+      if logl>bestLL
+          A=Ai; B=Bi; C=Ci; D=Di; Q=Qi; R=Ri; X=Xi; P=Pi;
+          bestLL=logl;
+          opts.targetLogL=bestLL;
+          disp(['Success, best logL=' num2str(bestLL,8)])
+      end
+    catch
+      1;
     end
 end
 
