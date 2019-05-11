5378eaa774c0959297eeaa6a920f4f98bf61fc56
Unknown <itu.pablo@gmail.com>
2019-05-11 14:02:43 -0400

    Speed improvement for info filter

--- kalman/trueStatInfoFilter.m
+++ kalman/trueStatInfoFilter.m
@@ -9,25 +9,18 @@ previ=i0;
 prevI=I0;
 
 %Pre-comp for efficiency:
-invertibleQ=all(diag(Q)>0);
-[ciQ,~,iQ]=pinvchol(Q);
+[ciQ,~,iQ]=pinvchol2(Q);
+invertibleQ=norm(Q*iQ,'fro')<1e-9;
+iA=pinv(A); %This is order-of-magnitude slower than inv(A)
+invertibleA=norm(A*iA,'fro')<1e-9;
 iQA=iQ*A;
 ciQA=ciQ'*A;
 AtiQA=ciQA'*ciQA;
 ey=eye(size(Q));
 
 %Init arrays:
-if isa(Y,'gpuArray') %For code to work on gpu
-    ip=nan(D1,N+1,'gpuArray');      ii=nan(D1,N,'gpuArray');
-    Ip=nan(D1,D1,N+1,'gpuArray');   I=nan(D1,D1,N,'gpuArray');
-else
-    ip=nan(D1,N+1);      ii=nan(D1,N);
-    Ip=nan(D1,D1,N+1);   I=nan(D1,D1,N);
-    if nargout>4
-        xp=zeros(D1,N+1);      xf=zeros(D1,N);
-        Pp=zeros(D1,D1,N+1);   Pf=zeros(D1,D1,N);
-    end
-end
+ip=nan(D1,N+1);      ii=nan(D1,N);
+Ip=nan(D1,D1,N+1);   I=nan(D1,D1,N);
 ip(:,1)=previ;
 Ip(:,:,1)=prevI;
 
@@ -44,68 +37,50 @@ for i=1:slowSamples
   ii(:,i)=previ;
   I(:,:,i)=prevI; %Store results
 
-  %Then, predict next step: (if prevI==0 there is no need for this)
-  properPriors=diag(prevI)~=0;
-  if any(~properPriors) && ~invertibleQ
-    warning('trueStatInfoFilter:nonInvQ','Q was not invertible but improper priors were given. Regularizing Q for invertibility')
-    %This is a workaround. The other alternative is to set improper priors
-    %to large but finite values, so we do not need the inverse of Q.
-    Q=Q+1e-7*eye(size(Q));
-    invertibleQ=true;
-    [ciQ,~,iQ]=pinvchol(Q);
-    iQA=iQ*A;
-    ciQA=ciQ'*A;
-    AtiQA=ciQA'*ciQA;
-  end
-  if any(~properPriors) && invertibleQ 
-      %This handles infinite variances well, as long as Q is invertible.
-      %As a trade-off, the computed covariances/information matrix may not
-      %be psd (difference of psd matrices).
-      [cholAuxP,~,auxP]=pinvchol2(prevI+AtiQA); %This may be problematic if Q has some 0 variances where we have 0 info.
-      HH=(ciQA*cholAuxP);
-      prevI=ciQ*(ey - HH*HH')*ciQ'; %I think this expression is better conditioned
-      previ=iQA*auxP*previ+prevI*BU(:,i);
-  elseif all(properPriors) %All proper priors, can do traditional Kalman update.
-      %This case is simple: invert I, compute updated P, invert P
-      [x,P]=info2state(previ,prevI); %Requires one pinvchol
-      [previ,prevI]=state2info(A*x+BU(:,i),A*P*A'+Q);
-  elseif ~invertibleQ %Case where some improper priors are present, 
-      %but Q is not invertible 
-      error('This does not work well. Infinity covariances may get propagated depending on structure of A')
-      %There has to be a solution to this problem, because the absence of
-      %state noise (non-invertible Q) cannot mean that we have LESS info
-      %about the states (i.e. for same system and invertible Q there is an
-      %MLE estimate).
-      %Further, the Kalman filter in the limit case were some covariance
-      %elements are very large tends to exist for any Q, so it is a matter
-      %of finding it.
+  %Then, predict next step:
+  if invertibleQ
+      if invertibleA
+          cI=mycholcov(prevI); %Could use chol() if prevI was guaranteed to be invertible, which has to be the case for invertibleA and after processing enough non-nan samples.
+          iAcI=iA'*cI;
+          [cP]=chol(iQ+iAcI*iAcI'); %Should always exist in this case
+          icP=ey/cP;
+          b=iAcI*icP;
+          prevI=iAcI*(ey - b*b')*iAcI';
+          previ=iQ*(icP*icP')*iA'*previ+prevI*BU(:,i);
+      else %prevI+A'*iQ*A needs to be invertible? Or can this be computed even on the PSD case?
+          [cholAuxP,~,auxP]=pinvchol(prevI+AtiQA); %Needs pinvchol2?
+          HH=(ciQA*cholAuxP);
+          prevI=ciQ*(ey - HH*HH')*ciQ'; %If prevI is not invertible, this may result in a non-PSD matrix because of numerical issues
+          previ=iQA*auxP*previ+prevI*BU(:,i);
+      end
+  elseif all(Q(:)==0)
+      if invertibleA
+          cI=mycholcov(prevI); %Could use chol() if prevI was guaranteed to be invertible, which has to be the case for invertibleA and after processing enough non-nan samples.
+          iAcI=iA'*cI;
+          prevI=iAcI*iAcI';
+          previ=iA'*previ+prevI*BU(:,i);
+      else %Some info will go to infinity! (0 uncertainty along some state subspace, numerically ugly)
+          [x,P]=info2state(previ,prevI); %Requires one pinvchol2
+          [previ,prevI]=state2info(A*x+BU(:,i),A*P*A'+Q); %Another pinvchol2
+      end
+  else %Q is PSD, unclear if the matrix inversion lemma can be used in any appropriate way, but I suspect that it can if prevI+AtiQA is invertible, and maybe even if it is not
+      %This is the defeatist approach, requires 2 full pinvchol2 to handle all possible situations, which is very slow: 
+      [x,P]=info2state(previ,prevI); %Requires one pinvchol2
+      [previ,prevI]=state2info(A*x+BU(:,i),A*P*A'+Q); %Another pinvchol2
       
-      %OLD idea:
-      %At least some info available: doing alternative way, which: 
-      %guarantees PSD, works with non-inv Q, and as a bonus computes P and x
-      %Con: if C'*inv(R)*C is under-rank (e.g. when dim of obs is less than
-      %dim of state), the inverse of prevI will contain infinite elements
-      %and this may be crap.
-      %NOTE: this does not work well if there are improper priors
-      [cOldP,~,oldP]=pinvchol2(prevI);
-      oldX=oldP*previ;
-      AcP=A*cOldP;
-      prevP=AcP*AcP'+Q;
-      aux=find(~properPriors);
-      prevP(aux,:)=0;
-      prevP(:,aux)=0;
-      prevP(sub2ind([D1,D1],aux,aux))=Inf;
-      prevX=A*oldX+BU(:,i);
-      [previ,prevI]=state2info(prevX,prevP); %Requires one pinvchol
+      %An alternative approach is to force Q to a barely-invertible case, and use the matrix inversion lemma:
+      warning('trueStatInfoFilter:nonInvQ','Q was not invertible but improper priors were given. Regularizing Q for invertibility')
+      %This is a workaround. The other alternative is to set improper priors
+      %to large but finite values, so we do not need the inverse of Q.
+      Q=Q+1e-7*eye(size(Q));
+      invertibleQ=true;
+      [ciQ,~,iQ]=pinvchol(Q);
+      iQA=iQ*A;
+      ciQA=ciQ'*A;
+      AtiQA=ciQA'*ciQA;
   end
-
   if nargout>2 %Store Xp, Pp if requested:
       ip(:,i+1)=previ;   Ip(:,:,i+1)=prevI;
-      if nargout>4
-          error('!') %This is not computed properly
-          xp(:,i+1)=prevX;   Pp(:,:,i+1)=prevP;
-          xf(:,i)=oldX;      Pf(:,:,i)=oldP;
-      end
   end
 end
 
@@ -118,11 +93,7 @@ oldI=prevI+CtRinvC; %Updated value
 %Store:
 Ip(:,:,slowSamples+1:N)=repmat(prevI,1,1,N-slowSamples);
 I(:,:,slowSamples+1:N)=repmat(oldI,1,1,N-slowSamples);
-if nargout>4
-    Pp(:,:,slowSamples+1:N)=repmat(prevP,1,1,N-slowSamples);
-    Pf(:,:,slowSamples+1:N)=repmat(oldP,1,1,N-slowSamples);
-end
-  
+
 for i=(slowSamples+1):N %Update states succesively
   y=Y(:,i); %Output at this step
   %Update:
@@ -140,10 +111,6 @@ for i=(slowSamples+1):N %Update states succesively
 
   if nargout>2 %Store Xp, Pp if requested:
       ip(:,i+1)=previ;  
-      if nargout>4
-          xp(:,i+1)=prevX;  
-          xf(:,i)=oldX;   
-      end
   end
 end
 
---
+++ misc/modelCheck.m
@@ -0,0 +1,11 @@
+function []=modelCheck(A,C,Q,R)
+
+%Is the system observable? controllable?
+%Is A invertible? diagonalizable?
+%Is Q PSD? (required)
+%Is Q invertible?
+%Is R PSD? (required)
+%Is R invertible? (required!)
+%Is C'*R*C invertible? (not needed, but 
+
+end
